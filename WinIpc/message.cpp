5a94a85c5aa007d01cc8506e0e54e120fc56f1e2
swiftlc <swiftlc@163.com>
2019-10-21 17:59:07 +0800

    + 添加 message

---
+++ WinIpc/message.cpp
@@ -0,0 +1,261 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cassert>
+#include <algorithm>
+
+#include "message.h"
+
+namespace {
+
+  const int kPayloadUnit = 32;
+
+  const size_t kCapacityReadOnly = static_cast<size_t>(-1);
+
+}
+
+namespace WinIpc {
+
+  const uint32 Message::kHeaderSize = sizeof(Message::Header);
+
+
+  Message::~Message() {
+    if (capacity_ != kCapacityReadOnly)
+      free(header_);
+  }
+
+  Message::Message()
+    : header_(NULL)
+    , capacity_(0) {
+    Resize(kPayloadUnit);
+
+    header()->payload_size = 0;
+    header()->routing = header()->type = 0;
+    header()->flags = 0;
+  }
+
+  Message::Message(int32 routing_id, uint32 type)
+    : header_(NULL)
+    , capacity_(0) {
+    Resize(kPayloadUnit);
+
+    header()->payload_size = 0;
+    header()->routing = routing_id;
+    header()->type = type;
+    header()->flags = 0;
+  }
+
+  Message::Message(const char* data, int data_len)
+    : header_(reinterpret_cast<Header*>(const_cast<char*>(data)))
+    , capacity_(kCapacityReadOnly) {
+
+    if (kHeaderSize > static_cast<unsigned int>(data_len))
+      header_ = NULL;
+
+    if (header_ && header_->payload_size + kHeaderSize > static_cast<unsigned int>(data_len))
+      header_ = NULL;
+  }
+
+  void Message::SetHeaderValues(int32 routing, uint32 type, uint32 flags) {
+    // This should only be called when the message is already empty.
+    assert(payload_size() == 0);
+
+    header()->routing = routing;
+    header()->type = type;
+    header()->flags = flags;
+  }
+
+  Message::Header* Message::header()
+  {
+    return static_cast<Header*>(header_);
+  }
+
+  const Message::Header* Message::header() const
+  {
+    return static_cast<const Header*>(header_);
+  }
+
+  bool Message::Resize(size_t new_capacity)
+  {
+    new_capacity = AlignInt(new_capacity, kPayloadUnit);
+
+    assert(capacity_ != kCapacityReadOnly);
+    void* p = realloc(header_, new_capacity);
+    if (!p)
+      return false;
+
+    header_ = reinterpret_cast<Header*>(p);
+    capacity_ = new_capacity;
+    return true;
+  }
+
+  const char* Message::FindNext(const char* range_start, const char* range_end)
+  {
+    if (static_cast<size_t>(range_end - range_start) < sizeof(Header))
+      return NULL;
+
+    const Header* hdr = reinterpret_cast<const Header*>(range_start);
+    const char* payload_base = range_start + sizeof(Header);
+    const char* payload_end = payload_base + hdr->payload_size;
+    if (payload_end < payload_base)
+      return NULL;
+
+    return (payload_end > range_end) ? NULL : payload_end;
+  }
+
+  bool Message::WriteString(const std::string& value)
+  {
+    if (!WriteInt(static_cast<int>(value.size())))
+      return false;
+
+    return WriteBytes(value.data(), static_cast<int>(value.size()));
+  }
+
+  bool Message::WriteString(const std::wstring& value)
+  {
+    if (!WriteInt(static_cast<int>(value.size())))
+      return false;
+
+    return WriteBytes(value.data(),
+      static_cast<int>(value.size() * sizeof(wchar_t)));
+  }
+
+  bool Message::WriteData(const char* data, int length)
+  {
+    return length >= 0 && WriteInt(length) && WriteBytes(data, length);
+  }
+
+  bool Message::WriteBytes(const void* data, int data_len)
+  {
+    assert(kCapacityReadOnly != capacity_);
+
+    size_t offset = header_->payload_size;
+
+    size_t new_size = offset + data_len;
+    size_t needed_size = sizeof(Header) + new_size;
+    if (needed_size > capacity_ && !Resize((std::max)(capacity_ * 2, needed_size)))
+      return false;
+
+    header_->payload_size = static_cast<uint32>(new_size);
+    char* dest = const_cast<char*>(payload()) + offset;
+    memcpy(dest, data, data_len);
+    return true;
+  }
+
+  MessageReader::MessageReader(Message* m)
+    : read_ptr_(m->payload())
+    , read_end_ptr_(m->end_of_payload())
+  {
+
+  }
+
+  template <typename Type>
+  inline bool MessageReader::ReadBuiltinType(Type* result) {
+    const char* read_from = GetReadPointerAndAdvance(sizeof(Type));
+    if (!read_from)
+      return false;
+    memcpy(result, read_from, sizeof(*result));
+    return true;
+  }
+
+  const char* MessageReader::GetReadPointerAndAdvance(int num_bytes) {
+    if (num_bytes < 0 || read_end_ptr_ - read_ptr_ < num_bytes)
+      return NULL;
+    const char* current_read_ptr = read_ptr_;
+    read_ptr_ += num_bytes;
+    return current_read_ptr;
+  }
+
+  const char* MessageReader::GetReadPointerAndAdvance(int num_elements, size_t size_element)
+  {
+    // Check for int32 overflow.
+    int64 num_bytes = static_cast<int64>(num_elements)* size_element;
+    int num_bytes32 = static_cast<int>(num_bytes);
+    if (num_bytes != static_cast<int64>(num_bytes32))
+      return NULL;
+    return GetReadPointerAndAdvance(num_bytes32);
+  }
+
+  bool MessageReader::ReadBool(bool* result)
+  {
+    return ReadBuiltinType(result);
+  }
+
+  bool MessageReader::ReadInt(int* result)
+  {
+    return ReadBuiltinType(result);
+  }
+
+  bool MessageReader::ReadUInt16(uint16* result)
+  {
+    return ReadBuiltinType(result);
+  }
+
+  bool MessageReader::ReadUInt32(uint32* result)
+  {
+    return ReadBuiltinType(result);
+  }
+
+  bool MessageReader::ReadInt64(int64* result)
+  {
+    return ReadBuiltinType(result);
+  }
+
+  bool MessageReader::ReadUInt64(uint64* result)
+  {
+    return ReadBuiltinType(result);
+  }
+
+  bool MessageReader::ReadFloat(float* result)
+  {
+    return ReadBuiltinType(result);
+  }
+
+  bool MessageReader::ReadString(std::string* result)
+  {
+    int len;
+    if (!ReadInt(&len))
+      return false;
+    const char* read_from = GetReadPointerAndAdvance(len);
+    if (!read_from)
+      return false;
+
+    result->assign(read_from, len);
+    return true;
+  }
+
+  bool MessageReader::ReadWString(std::wstring* result)
+  {
+    int len;
+    if (!ReadInt(&len))
+      return false;
+    const char* read_from = GetReadPointerAndAdvance(len, sizeof(wchar_t));
+    if (!read_from)
+      return false;
+
+    result->assign(reinterpret_cast<const wchar_t*>(read_from), len);
+    return true;
+  }
+
+  bool MessageReader::ReadData(const char** data, int* length)
+  {
+    *length = 0;
+    *data = 0;
+
+    if (!ReadInt(length))
+      return false;
+
+    return ReadBytes(data, *length);
+  }
+
+  bool MessageReader::ReadBytes(const char** data, int length)
+  {
+    const char* read_from = GetReadPointerAndAdvance(length);
+    if (!read_from)
+      return false;
+    *data = read_from;
+    return true;
+  }
+
+}  // namespace IPC
---
+++ WinIpc/message.h
@@ -0,0 +1,164 @@
+#pragma once
+
+#include <string>
+
+#include "common.h"
+
+namespace WinIpc {
+
+  class Message;
+
+  class MessageReader
+  {
+  public:
+    MessageReader() : read_ptr_(NULL), read_end_ptr_(NULL) {}
+    explicit MessageReader(Message* m);
+
+    bool ReadBool(bool* result);
+    bool ReadInt(int* result);
+    bool ReadUInt16(uint16* result);
+    bool ReadUInt32(uint32* result);
+    bool ReadInt64(int64* result);
+    bool ReadUInt64(uint64* result);
+    bool ReadFloat(float* result);
+    bool ReadString(std::string* result);
+    bool ReadWString(std::wstring* result);
+    bool ReadData(const char** data, int* length);
+    bool ReadBytes(const char** data, int length);
+
+  private:
+    template <typename Type>
+    inline bool ReadBuiltinType(Type* result);
+
+    const char* GetReadPointerAndAdvance(int num_bytes);
+
+    inline const char* GetReadPointerAndAdvance(int num_elements,
+      size_t size_element);
+
+    const char* read_ptr_;
+    const char* read_end_ptr_;
+  };
+
+  class Message {
+  public:
+    Message();
+
+    Message(int32 routing_id, uint32 type);
+
+    Message(const char* data, int data_len);
+
+    ~Message();
+
+    // Returns the size of the Pickle's data.
+    size_t size() const { return kHeaderSize + header_->payload_size; }
+
+    // Returns the data for this Pickle.
+    const void* data() const { return header_; }
+
+    // The payload is the pickle data immediately following the header.
+    size_t payload_size() const { return header_->payload_size; }
+
+    const char* payload() const {
+      return reinterpret_cast<const char*>(header_) + kHeaderSize;
+    }
+
+    // Returns the address of the byte immediately following the currently valid
+    // header + payload.
+    const char* end_of_payload() const {
+      // This object may be invalid.
+      return header_ ? payload() + payload_size() : NULL;
+    }
+
+    uint32 type() const {
+      return header()->type;
+    }
+
+    int32 routing_id() const {
+      return header()->routing;
+    }
+
+    void set_routing_id(int32 new_id) {
+      header()->routing = new_id;
+    }
+
+    // Sets all the given header values. The message should be empty at this
+    // call.
+    void SetHeaderValues(int32 routing, uint32 type, uint32 flags);
+
+    bool WriteBool(bool value) {
+      return WriteInt(value ? 1 : 0);
+    }
+
+    bool WriteInt(int value) {
+      return WriteBytes(&value, sizeof(value));
+    }
+
+    bool WriteUInt16(uint16 value) {
+      return WriteBytes(&value, sizeof(value));
+    }
+
+    bool WriteUInt32(uint32 value) {
+      return WriteBytes(&value, sizeof(value));
+    }
+
+    bool WriteInt64(int64 value) {
+      return WriteBytes(&value, sizeof(value));
+    }
+
+    bool WriteUInt64(uint64 value) {
+      return WriteBytes(&value, sizeof(value));
+    }
+
+    bool WriteFloat(float value) {
+      return WriteBytes(&value, sizeof(value));
+    }
+
+    bool WriteString(const std::string& value);
+
+    bool WriteString(const std::wstring& value);
+
+    // "Data" is a blob with a length. When you read it out you will be given the
+    // length. See also WriteBytes.
+    bool WriteData(const char* data, int length);
+
+    // "Bytes" is a blob with no length. The caller must specify the lenght both
+    // when reading and writing. It is normally used to serialize PoD types of a
+    // known size. See also WriteData.
+    bool WriteBytes(const void* data, int data_len);
+
+    // 从buffer中寻找下一条消息的起始地址
+    static const char* FindNext(const char* range_start, const char* range_end);
+  protected:
+    friend class Channel;
+
+#pragma pack(push, 4)
+    struct Header {
+      int32 routing;  // ID of the view that this message is destined for
+      uint32 type;    // specifies the user-defined message type
+      uint32 flags;   // specifies control flags for the message
+      uint32 payload_size;
+    };//16 BYTES
+#pragma pack(pop)
+
+    Header* header();
+    const Header* header() const;
+
+    // Resize the capacity, note that the input value should include the size of
+    // the header: new_capacity = sizeof(Header) + desired_payload_capacity.
+    // A realloc() failure will cause a Resize failure... and caller should check
+    // the return result for true (i.e., successful resizing).
+    bool Resize(size_t new_capacity);
+
+    // Aligns 'i' by rounding it up to the next multiple of 'alignment'
+    static size_t AlignInt(size_t i, int alignment) {
+      return i + (alignment - (i % alignment)) % alignment;
+    }
+
+    static const uint32 kHeaderSize;
+
+    Header* header_;
+    // Allocation size of payload (or -1 if allocation is const).
+    size_t capacity_;
+  };
+
+}
